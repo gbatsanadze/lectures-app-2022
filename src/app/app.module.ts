0f54b10358606d1af006122739b9655263b9c205
tamari <tamari.sumbadz2@gmail.com>
2022-05-27 13:08:21 +0400

    Revert "seminar-3-ts"
    This reverts commit 3c8508b17dc8a0cb259ccff56b3c1d84152c96ee.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
