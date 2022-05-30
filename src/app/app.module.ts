d49b1e14feaf214a78f72bc6f236145e750408de
George <giorgibokuchava1@gmail.com>
2022-05-31 01:11:12 +0400

    Revert "gb-seminar-4"
    This reverts commit 90b89d2d5cc742affff691f06440067fce0aea60.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BgPopupDirective } from './bg-popup.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BgPopupDirective
+    AppComponent
   ],
   imports: [
     BrowserModule
