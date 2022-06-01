fa040fbd81ca1c7ecab7f581a071f5eca9a71903
EkaK <kakalashvilieka0@gmail.com>
2022-06-02 00:18:21 +0400

    Revert "EK - seminar-6"
    This reverts commit a347d79f819808329c72c1b0b2dd214124168580.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'counter',
-    component: CounterComponent
-  }
-];
+
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CounterComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
