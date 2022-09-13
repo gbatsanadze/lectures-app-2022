1bfba3d256eb90e7b72ec3dcc150dc52b82e31f4
Kristina <k_charkviani@cu.edu.ge>
2022-09-13 21:30:36 +0400

    initial commit

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShellComponent implements OnInit {
 
+  userName = '';
+
+  onInput(event: Event) {
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
