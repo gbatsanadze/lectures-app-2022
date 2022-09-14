cb22a92d5a19574ad43c2edf013946a3e6658387
Kristina <k_charkviani@cu.edu.ge>
2022-09-14 23:43:59 +0400

    initial commit

---
+++ src/app/bg-popup.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BgPopupDirective } from './bg-popup.directive';
+
+describe('BgPopupDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BgPopupDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/bg-popup.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[bgBgPopup]'
+})
+export class BgPopupDirective {
+
+  constructor() { }
+
+}
