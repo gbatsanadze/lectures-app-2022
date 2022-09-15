90a05b205434df596cf52c3e89c10649894c80fb
Gvantsa <melitaurigvantsa@gmail.com>
2022-09-15 18:50:42 +0400

    Auto stash before rebase of "refs/heads/gvmelitauri-seminar-3"

---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.scss']
+})
+export class GameControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onStart(){
+
+  }
+
+  onEnd(){
+
+
+ }
+}
