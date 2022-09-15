6b1b40b71b87f5261cafd58a5a86de863d48b784
Gvantsa <gvantsa.khomasuridze@gmail.com>
2022-09-15 23:15:40 +0400

    commit

--- src/app/changeUserLocation.service.ts
+++ src/app/changeUserLocation.service.ts
@@ -1,16 +1,22 @@
 import {Injectable} from '@angular/core';
+import {CounterService} from "./counter.service";
 
 @Injectable({ providedIn: 'root'})
 export class ChangeUserLocationService{
   activeUsers = ['ლევანი', 'დიმა'];
   inactiveUsers = ['მარიამი', 'გიორგი'];
 
+  constructor(private counterService: CounterService) {
+  }
+
   onSetToInactive(id: number) {
     this.inactiveUsers.push(...this.activeUsers.splice(id, 1));
+    this.counterService.count();
   }
 
   onSetToActive(id: number) {
     this.activeUsers.push(...this.inactiveUsers.splice(id, 1));
+    this.counterService.count();
   }
 
 }
