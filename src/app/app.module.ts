0cfdd9849387a9ea1bf5ebc3f97b3d4ec547a64f
nana <nsanikidze@bog.ge>
2022-09-19 01:06:29 +0400

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ShellComponent } from './shell/shell.component';
     ShellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -15,20 +15,6 @@ export class ShellComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getUserName(item){
-      this.userName = (item.target as HTMLInputElement).value;
-      if (this.userName === ''){
-        this.allowButtonClick = false;
-      }else {
-        this.allowButtonClick = true;
-      }
-
-  }
-
-  addUser(){
-    this.userName = '';
-    this.allowButtonClick = false;
-  }
 
   dissapear(){
     this.allowAddButtonClick = !this.allowAddButtonClick;
