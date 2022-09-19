eb937b881c3a1e2b7b6f2eb73fededc5cc907ec1
nana <nsanikidze@bog.ge>
2022-09-20 01:26:44 +0400

    done

--- src/app/directives/popup.directive.ts
+++ src/app/directives/popup.directive.ts
@@ -5,39 +5,19 @@ import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} fro
 })
 export class PopupDirective implements OnInit {
 
-  isOpenV = false;
 
-  @HostBinding('class.open') isOpen = this.isOpenV;
+  @HostBinding('class.open') isOpen = false;
 
-  constructor(private elementRef: ElementRef,
-              private renderer: Renderer2){
+  constructor(){
 
   }
   ngOnInit() {
   }
 
   @HostListener('click') click(){
-    console.log(1);
-    if (this.isOpenV) {
-      console.log(2);
-      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
-    } else {
-      console.log(3);
-      this.renderer.addClass(this.elementRef.nativeElement, 'open');
-    }
-    this.isOpenV = !this.isOpenV;
+    this.isOpen = !this.isOpen;
   }
 
-
-  @HostListener('document:click') documentClick(){
-    console.log(4);
-    if (!this.isOpenV) {
-      console.log(5);
-      console.log(5);
-      this.renderer.removeClass(this.elementRef.nativeElement, 'open');
-    }
-
-  }
 
 }
 
