2bd0bce0a3b12aedcd9602d74514ee4f36951213
Gvantsa <gvantsa.khomasuridze@gmail.com>
2022-09-21 16:08:49 +0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {OddComponent} from "./odd/odd.component";
+import {OddComponent} from './odd/odd.component';
+import {GameControlComponent} from './game-control/game-control.component';
+import {EvenComponent} from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OddComponent
+    OddComponent,
+    GameControlComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
