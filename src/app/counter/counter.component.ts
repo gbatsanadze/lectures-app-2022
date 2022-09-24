c1cff232e794e770b9f969254c1575e0b986e47b
nana <nsanikidze@bog.ge>
2022-09-24 20:33:58 +0400

    commit 2

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscriber, Subscription} from "rxjs";
 
 @Component({
   selector: 'bg-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, OnDestroy{
   counter = 0;
+  subscribeParam: Subscription;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute ,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.subscribeParam = this.activatedRoute.queryParams.subscribe( params => {
+      if (!isNaN(params.counter)){
+        this.counter = params.counter;
+      }
+    });
 
-    if (this.activatedRoute.snapshot.queryParams.counter !== undefined){
-      this.counter = this.activatedRoute.snapshot.queryParams.counter;
-    }
+  }
+
+  ngOnDestroy() {
+    this.subscribeParam.closed;
   }
 
   increaseCounter(){
     this.counter++;
+    this.router.navigate(['/counter'], {queryParams: {counter: this.counter}});
   }
 
 }
