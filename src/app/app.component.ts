31cadc6881c71296c2dd33a3e406dcfdfceaad22
nana <nsanikidze@bog.ge>
2022-09-28 22:21:13 +0400

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SeminarService } from './seminar.service';
+import {Observable, Subscription} from "rxjs";
+import {timeout} from "rxjs/operators";
 
 @Component({
   selector: 'bg-root',
@@ -9,10 +11,20 @@ import { SeminarService } from './seminar.service';
 })
 export class AppComponent implements OnInit {
   showCongratulations = false;
+  observeble: Subscription;
 
   constructor(private seminarService: SeminarService) {}
 
   ngOnInit() {
     // TODO: აქ დაწერეთ თქვენი interval observable-ის კოდი.
+
+    const myTimeOut = num =>  new Observable<number>( observer => {
+      let counter = 0;
+      setTimeout(() => {
+        observer.next(counter++);
+      }, num);
+    });
+
+
   }
 }
